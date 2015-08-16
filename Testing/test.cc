2b103a5a1753b4db979334b6f4e9ab8d5bf62ce8
Gregg Rivera <gregg-rivera@outlook.com>
2015-08-16 18:13:58 -0500

    revised test.cc for current project status

--- Testing/test.cc
+++ Testing/test.cc
@@ -5,9 +5,16 @@ int main(void){
         /*
          * This program is testing binary encoding for swf file format
          * using the example given in the swfdocumentation pdf in the 
-         * Documents folder, Apendex A. It does not work and I believe 
-         * the problem is based on big/little endian encoding or the hex
-         * is not being translated to binary properly
+         * Documents folder, Apendex A. After installing gnash and 
+         * confirming that other swf files do indeed work, this program
+         * was successful in producing a flash slide that showcases a single
+         * rectangle. This means that using the flash manual, we can write
+         * our own complier and flash program using objects tied to GTK+'s
+         * GUI system to make a full import/export flash development tool.
+         * I will leave the compiled .swf file in the folder as I need 
+         * confirmation of it working on a windows system (just to be 
+         * certain there aren't any weird discrepancies. Also I will
+         * leave the hex dump.
          */
 
         char buffer[79] =
@@ -22,7 +29,7 @@ int main(void){
         0x00,0xB4,0x86,0x00,0x06,0x06,0x00,0x01,
         0x00,0x01,0x40,0x00,0x00,0x00,0x00};
 
-        std::ofstream fout ("action.swf", std::ios::binary);
+        std::ofstream fout ("test01.swf", std::ios::binary);
         fout.write(buffer,79);
         std::cout<<"value written"<<std::endl;
         fout.close();
